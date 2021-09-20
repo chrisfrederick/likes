1b33aa2ff5efd2537e3b7138765c5e4f1c5d1da7
Chris Frederick <chrisfrederick2008@gmail.com>
2021-09-20 16:08:25 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var add1 = 9;
+var add2 = 12;
+var add3 = 9;
+
+
+
+function raiseLikes(){
+    add1++;
+    document.querySelector("#likecount1").innerText = add1 + " Like(s)";
+    
+}
+
+function raiseLikes2(){
+    add2++;
+    document.querySelector("#likecount2").innerText = add2 + " Like(s)";
+    
+}
+
+function raiseLikes3(){
+    add3++;
+    document.querySelector("#likecount3").innerText = add3 + " Like(s)";
+    
+}
